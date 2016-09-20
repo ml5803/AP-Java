9579a14ece11c36eae930236d12bdc77c79030f1
TOSHIBA <TOSHIBA@TOSHIBA-PC>
2016-09-20 19:54:58 -0400

    unfairDice

---
+++ UnfairDice/src/unfairDiceSimulator/UnfairDiceObject.java
@@ -0,0 +1,10 @@
+package unfairDiceSimulator;
+
+public class UnfairDiceObject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
