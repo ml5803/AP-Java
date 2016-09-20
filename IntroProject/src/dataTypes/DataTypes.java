c8b2de053f10724178015b7367a7161ff21144af
Student 7 <Student 7@10.8.33.168>
2016-09-20 12:22:38 -0400

    testing

--- IntroProject/src/dataTypes/DataTypes.java
+++ IntroProject/src/dataTypes/DataTypes.java
@@ -5,13 +5,24 @@ public class DataTypes {
 	public DataTypes() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public static void main(String[] args){
-		System.out.println(5.0/2);//double divided by int
-		System.out.println((double)5/2);//casting
-		System.out.println(5/2);
-		System.out.println(3+5.0/2+5*2);
-		System.out.println(3.0+5/2+5*2);
-		//hi
+		//	System.out.println(5.0/2);//double divided by int
+		//	System.out.println((double)5/2);//casting
+		//	System.out.println(5/2);
+		//	System.out.println(3+5.0/2+5*2);
+		//	System.out.println(3.0+5/2+5*2);
+
+
+		//		double d1 = 4.64; 
+		//		double d2 = 2.0;
+		//		double d3 = 2.64;
+
+		//		System.out.println("d1 : " + d1); 
+		//		System.out.println("d2 : " + d2); 
+		//		System.out.println("d3 : " + d3);  
+		//		System.out.println("d1 - d2 : " + (d1 - d2));
+		//		
+		//		if(42 == 42.0) System.out.println("42 equals 42.0");
 	}
 }
