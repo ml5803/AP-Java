5352fe14ff71f18db23b8f3ab0d6d870aac2d6e2
Student 7 <Student 7@10.8.33.168>
2016-10-11 12:58:18 -0400

    arrays

---
+++ Arrays/src/ArrayMain.java
@@ -0,0 +1,15 @@
+public class ArrayMain {
+
+	public static void main(String[] args) {
+		//construct 2 integer arrays
+		//those two methods do the same thing
+		//primative type arrays fill with ZEROES: 0,0.0,false;
+		int[] zeros1= {0,0,0};
+		int[] zeros2 = new int[3];
+		
+		//these two constructors are different
+		String[] strings1={"","",""};
+		String[] strings2=new String[3];
+	}
+
+}
