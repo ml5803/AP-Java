8c5cca761198dd5d094babba736113379d87708d
Student 7 <Student 7@10.8.33.168>
2016-11-02 13:19:28 -0400

    birds

--- Arrays/src/Practice.java
+++ Arrays/src/Practice.java
@@ -4,15 +4,61 @@ public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[][] arr2D= new String[10][8];
-	
+		String[][] pic= new String[10][12];
+
 		//print each row
-		for(int i = 0; i<arr2D.length; i++){
-			for (int j = 0; j<arr2D[i].length; j++){
-				System.out.print(arr2D[i][j]);
+		for(int row = 1; row<pic.length; row++){
+			for (int col = 0; col<pic[row].length; col++){
+				pic[row][col] = "           ";
 			}
-			System.out.println("\n" + " ");
+		}
+		
+		for(int row = 7; row<pic.length; row++){
+			for (int col = 0; col<pic[row].length; col++){
+				pic[row][col] = "MMM";
+			}
+		}
+		
+		//birds
+		int count = pickRandom(5,0);
+		for(int i = 0; i< count; i++){
+			pic[pickRandom(pic.length-3,0)][pickRandom(pic[0].length-5,3)] = "<-(o v o)->";
+		}
+		
+		//top row is "_"
+		for (int col = 0; col < pic[0].length;col++){
+			pic[0][col]="_";
+			pic[pic.length-1][col]= "_";
+		}
+		
+		for(int row = 0;row<pic.length;row++){
+			pic[row][0]="|";
+			pic[row][pic[0].length-1]="|";
+		}
+		
+		pic[1][2] = "O";
+		pic[0][2] = "|";
+		pic[2][2] = "|";
+		pic[1][1] = "-";
+		pic[1][3] = "-";
+		pic[0][1] = "\\";
+		pic[2][3] = "\\";
+		pic[0][3] = "/";
+		pic[2][1] = "/";
+		printPic(pic);
+	}
+	
+	public static int pickRandom(int upper, int lower){
+		return (int)(Math.random()*upper) + lower;
+	}
+	
+
+	public static void printPic(String[][] arr){
+		for(int i = 0; i<arr.length; i++){
+			for (int j = 0; j<arr[i].length; j++){
+				System.out.print(arr[i][j]);
+			}
+			System.out.println();
 		}
 	}
-
 }
